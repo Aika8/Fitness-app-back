aabbde0f61c3df58cea38ce2b6e120523276e472
Aika <ATumbayeva@one.kz>
2022-02-19 01:45:04 +0600

    test to fill data added

--- src/main/java/spring/first/fitness/entity/ElasticPost.java
+++ src/main/java/spring/first/fitness/entity/ElasticPost.java
@@ -8,7 +8,6 @@ import org.springframework.data.annotation.Id;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
 import org.springframework.data.elasticsearch.annotations.FieldType;
-import org.springframework.data.elasticsearch.annotations.Setting;
 import spring.first.fitness.helpers.ElasticConstants;
 
 @Data
@@ -16,7 +15,6 @@ import spring.first.fitness.helpers.ElasticConstants;
 @AllArgsConstructor
 @Builder
 @Document(indexName = ElasticConstants.POST_INDEX)
-@Setting(settingPath = "static/es-settings.json")
 public class ElasticPost {
     @Id
     @Field(type = FieldType.Keyword)
